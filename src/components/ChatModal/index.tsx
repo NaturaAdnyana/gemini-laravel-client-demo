81304a197b0a2198ded68fb305392fc0a49f31c5
NaturaAdnyana <natura538@gmail.com>
2024-06-14 14:00:01 +0800

    better back to http

--- src/components/ChatModal/index.tsx
+++ src/components/ChatModal/index.tsx
@@ -77,7 +77,7 @@ export default function index() {
     setIsLoaded(false);
 
     axios
-      .post("https://eventnya.id:50/webhooks/rest/webhook", {
+      .post("http://eventnya.id:50/webhooks/rest/webhook", {
         sender: user,
         message: message,
       })
